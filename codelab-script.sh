5da2c7c6315783d37b48d6a27f39b57fc488ea58
Thomas Saison <thomas.saison@octo.com>
2023-05-17 10:43:56 +0200

    add covergae

--- codelab-script.sh
+++ codelab-script.sh
@@ -3,7 +3,7 @@
 brew install go
 
 export GOPATH=$HOME/Go
-export GOROOT=/usr/local/opt/go/libexec
+# export GOROOT=/usr/local/opt/go/libexec
 export PATH=$PATH:$GOPATH/bin
 export PATH=$PATH:$GOROOT/bin
 
